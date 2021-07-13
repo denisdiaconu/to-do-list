09ba21f1710191d606b0174da05b869ccdee269f
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-07-13 21:56:42 +0200

    Add normal array

--- src/index.js
+++ src/index.js
@@ -1,44 +1,33 @@
 import './style.css';
-import { v4 as uuidv4 } from 'uuid';
 
-const toDoInput = document.querySelector('.toDoInput');
-const toDoButton = document.querySelector('.toDoButton');
 const toDoList = document.querySelector('#toDoList');
 
-const handleGetData = () => {
-  const list = localStorage.getItem('list');
-  if (list) {
-    return JSON.parse(list);
-  }
-  return [];
-};
-
-const handleSave = (data) => {
-  const list = handleGetData();
-  list.push(data);
-  localStorage.setItem('list', JSON.stringify(list));
-};
-
-toDoButton.addEventListener('click', () => {
-  const newTask = toDoInput.value;
-  const newTaskOb = {
-    description: newTask,
+const list = [
+  {
+    description: 'have fun',
     completed: false,
-    id: uuidv4(),
-  };
-  handleSave(newTaskOb);
-  window.location.reload();
-});
+    id: 1,
+  },
+  {
+    description: 'study a lot',
+    completed: false,
+    id: 2,
+  },
+  {
+    description: 'learn more',
+    completed: false,
+    id: 3,
+  },
+];
 
-const newlist = handleGetData();
-for (let i = 0; i < newlist.length; i += 1) {
+for (let i = 0; i < list.length; i += 1) {
   const li = document.createElement('li');
   li.classList.add('toDoElement');
   li.innerHTML = `
-      <div id='${newlist[i].id}'>
+      <div id='${list[i].id}'>
       <input type="checkbox" class="checkboxes"/>
-      <p>${newlist[i].description}</p>
-      <button class="btn" id="${newlist[i].id}")"><i class="fas fa-ellipsis-v"></i></button>
+      <p>${list[i].description}</p>
+      <button class="btn" id="${list[i].id}")"><i class="fas fa-ellipsis-v"></i></button>
       </div>`;
   toDoList.appendChild(li);
 }
