d69c9fb74a5a729fa6bcca16cdec16f0eb624cfd
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-07-13 14:24:40 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      template: './src/index.html'
+    }),
+
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+
+    contentBase: './dist',
+
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+};
